6a72046ea277e2fd1af9f151549e844eb82ccc1c
layerpie <garethdominic112@gmail.com>
2022-01-13 20:46:39 +0700

    Pertemuan 7 - Penggunaan atribute url pada request

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 var http = require('http');
 
 http.createServer((req,res)=>{
+    if (req.url == '/home'){
+        res.write('<p>Konten Home</p>')
+    }else if (req.url == '/product'){
+        res.write('<p>Konten Product</p>')
+    }else if (req.url == '/order'){
+        res.write('<p>Konten Order</p>')
+    res.end();
+    }else if (req.url == '/'){
+        res.write('<p>Pengembangan Aplikasi Web dengan Nodejs</p>')
+    }else
     res.end();
 }).listen(8000,()=>{
     console.log('Server is running on port 8000')
-})
+});
